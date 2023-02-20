81df40ac4ef16864742080d70852036ea8a88ec1
karapal <106742331+karapal@users.noreply.github.com>
2023-02-20 16:49:37 +0100

    sidebar not done

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,5 +1,13 @@
+import { Col } from "react-bootstrap";
+import RightBar from "./RightBar";
+
 const ProfilePage = () => {
-  return <h1>Test</h1>;
+  return  <> 
+   <Col lg={2} className=" mt-5 p-20 ">
+            <RightBar />
+    </Col>
+  
+</>
 };
 
 export default ProfilePage;
