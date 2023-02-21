30ef96414c20e9b1679c261edf13e6406d48d8c2
Flovedev <flove.dev@gmail.com>
2023-02-21 09:03:46 +0000

    updates

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,14 +1,19 @@
 import FooterComponent from "./FooterComponent";
 import NavbarComponent from "./NavbarComponent";
-
 import LowerProfile from "./LowerProfile";
 import MainProfile from "./MainProfile";
-
-import { Col } from "react-bootstrap";
 import RightBar from "./RightBar";
 
 const ProfilePage = () => {
-
+  return (
+    <>
+      <NavbarComponent />
+      <MainProfile />
+      <RightBar />
+      <LowerProfile />
+      <FooterComponent />
+    </>
+  );
 };
 
 export default ProfilePage;
