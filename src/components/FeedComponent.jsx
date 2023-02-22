644bbadc1f5d39f5862b5b0752494910442d9f44
Erik <erik.ondra@yahoo.com>
2023-02-22 15:18:16 +0100

    css adjustemts

--- src/components/FeedComponent.jsx
+++ src/components/FeedComponent.jsx
@@ -1,11 +1,25 @@
 import FeedLeftSideBar from "./FeedLeftSideBar";
-import { Container } from "react-bootstrap";
-
 import HomeRightBar from "./HomeRightBar";
 import PostSection from "./PostSection";
 import { Container, Row, Col } from "react-bootstrap";
 const FeedComponent = () => {
-  return <div className="feed-container"></div>;
+  return (
+    <Container className="feed-container">
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <Col>
+            <FeedLeftSideBar />
+          </Col>
+          <div className="mx-5 ">
+            <PostSection />
+          </div>
+          <div className="feed-right-bar-container">
+            <HomeRightBar />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default FeedComponent;
