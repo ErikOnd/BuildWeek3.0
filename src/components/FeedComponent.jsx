9184c16fc57e51f507f2c555af1419bd3f287ce7
Sabin789 <87514484+Sabin789@users.noreply.github.com>
2023-02-22 14:07:03 +0000

    Added post and rightbar on the home page

--- src/components/FeedComponent.jsx
+++ src/components/FeedComponent.jsx
@@ -1,5 +1,20 @@
+import HomeRightBar from "./HomeRightBar";
+import PostSection from "./PostSection";
+import{Container,Row,Col} from "react-bootstrap"
 const FeedComponent = () => {
-  return <div className="feed-container"></div>;
+  return <Container className="feed-container">
+    <Row>
+    <Col className="d-flex justify-content-center">
+      <div className="mx-5 ">
+    <PostSection />
+    </div>
+    <div className="feed-right-bar-container">
+   <HomeRightBar />
+   </div>
+
+   </Col>
+   </Row>
+  </Container>;
 };
 
 export default FeedComponent;
--- src/components/HomeRightBar.jsx
+++ src/components/HomeRightBar.jsx
@@ -1,11 +1,14 @@
 import { Container,Row,Col } from "react-bootstrap";
 import { useState } from "react";
+import { ArrowDown,ArrowUp } from "react-bootstrap-icons";
+// import {logo} from "../Css/pics/Ll-Logo"
 const HomeRightBar = () => {
     let [more,setMore]=useState(false)
-    return ( <Container className="home-rigthbar-container">
+    return ( <><Container className="home-rigthbar-container">
      <Row>
-        <div className="d-flex">
-            <p>LinkedIn News</p>
+        <div className="d-flex ">
+            <p className="ml-4"><b> LinkedIn News</b></p>
+         
         </div>
         </Row>
         <div className="side-news">
@@ -50,9 +53,40 @@ const HomeRightBar = () => {
 
         <small>1d ago • 1,698 readers</small>
         </div>
-        <button onClick={()=>setMore(false)}>Show Less</button>
-        </> :    <button onClick={()=>setMore(true)}>Show More</button>}
-    </Container> );
+        <button onClick={()=>setMore(false)} className="ml-4 "> Show Less <ArrowUp/></button>
+        </> :    <button onClick={()=>setMore(true)} className="ml-4 ">Show More <ArrowDown className=" arrowDown"/></button>   }
+    </Container>
+ 
+        <Container className="home-rightbar-footer">
+            <div className="home-rightbar-footer-links">
+                <Row className="d-flex justify-content-center">
+          <small><a className="rightbar-link" href="https://about.linkedin.com/">About</a></small>
+          <small ><a className="rightbar-link" href="https://www.linkedin.com/accessibility">Accessibility</a></small>
+          <small ><a className="rightbar-link" href="https://www.linkedin.com//help/linkedin?trk=footer_d_flagship3_feed">Help Center</a></small>
+          </Row>
+          <Row className="d-flex justify-content-center">
+          <small ><a className="rightbar-link" href="">Privacy & Terms <ArrowDown/> </a></small>
+          <small ><a className="rightbar-link" href="https://www.linkedin.com/help/linkedin/answer/a1342443">Ad Choices</a></small>
+          </Row>
+          
+         
+          <Row className="d-flex justify-content-center">
+          <small ><a className="rightbar-link" href="https://business.linkedin.com/marketing-solutions/ads?trk=n_nav_ads_rr_b&src=li-nav&veh=ad%2Fstart">Advertising</a></small>
+          <small ><a className="rightbar-link" href="">Business Services <ArrowDown/></a> </small>
+         
+          </Row>
+          <Row className="d-flex justify-content-center">
+          <small ><a className="rightbar-link" href="">Get the LinkedIn app</a></small>
+          <small ><a className="rightbar-link" href="">More</a></small>
+          </Row>
+          </div>
+          <br />
+          <Row className="d-flex justify-content-center">
+          <small className=""> LinkedIn Corporation © 2023</small>
+          </Row>
+        </Container>
+
+    </> );
 }
  
 export default HomeRightBar;
